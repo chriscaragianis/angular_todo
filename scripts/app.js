ce5cf2ed9e7a4f1a0453bf6048a202af1f604e04
Chris Caragianis <chriscaragianis@gmail.com>
2016-05-17 22:09:15 -0400

    End lesson 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope){
+  $scope.helloWorld = function() {
+    console.log("Hello there");
+  };
+
+  $scope.todos = [
+    {"name": "Do this thing"},
+    {"name": "Do this other thing"},
+    {"name": "Do this stupid thing"},
+    {"name": "Do this fun thing"},
+    {"name": "Do this boring thing"},
+    {"name": "Do this last thing"}
+  ];
+});
